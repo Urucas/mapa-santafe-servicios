28e40710c2d19c81108f76da57001f630a637ab5
vrunoa <vruno.alassia@gmail.com>
2015-03-12 12:09:05 -0300

    add controller.js

---
+++ controller.js
@@ -0,0 +1,71 @@
+var app = new (function app() {
+  
+  var self = this;
+  this.map;
+  this.geocoper;
+  this.container = document.getElementById("map-container");
+  this.servicios = servicios;
+  this.interval;
+
+  this.getScreenSize = function() {
+    return { 
+      h: window.screen.availHeight,
+      w: window.screen.availWidth
+    }
+  }
+
+  this.initialize = function() {
+
+    self.container.style.height = (self.getScreenSize().h)+"px";
+
+    var mapOptions = {
+      center: { lat:-32.9458328, lng: -60.6691576},
+      zoom: 15,
+      panControl: true,
+      zoomControl: true,
+      scaleControl: true
+    };
+    self.map = new google.maps.Map(self.container, mapOptions);
+
+    self.geocoder = new google.maps.Geocoder();
+    self.getGeocodes();
+    
+  }
+
+  this.getGeocodes = function() {
+    self.interval = setInterval(function(){
+      var s = self.getNextServicio();
+      if(!s) {
+        clearInterval(self.interval);
+        return;
+      }
+      var addr = s[1] + ",Rosario,Santa Fe, Argentina";
+      self.getGeo(addr, function(loc){
+        if(!loc) return;
+        var marker = new google.maps.Marker({
+          map: self.map,
+          position: loc
+        });
+      });
+    }, 1000);
+  }
+
+  this.getNextServicio = function() {
+    return self.servicios.length ? self.servicios.shift() : false;
+  }
+
+  this.getGeo = function(address, cb) {
+    self.geocoder.geocode({ 'address': address}, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        // map.setCenter(results[0].geometry.location);
+        cb(results[0].geometry.location);
+      } else {
+        console.log(address+ " error: "+ status);
+        cb(false);
+      }
+    });
+  }
+
+})();
+
+app.initialize();  
